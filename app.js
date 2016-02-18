a829dc0815ef7e3cde7982d3fdddfd0e44c1c184
Amanda Milke <amandalmilke@gmail.com>
2016-02-18 16:28:31 -0500

    made stuff happen

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 $(function(){
 
 	$.get('https://www.reddit.com/r/aww/.json', function(somethingElse){
+
+
 	//Makes the Sections
 		function createDivs() {
 			for(i = 0; i < 10; i++) {
-					// $(".x1").append(x3);
 				var title = somethingElse.data.children[i].data.title;
 				var imgUrl = somethingElse.data.children[i].data.url;
+				var author = somethingElse.data.children[i].data.author;
+				var comments = somethingElse.data.children[i].data.permalink;
+				var numComments = somethingElse.data.children[i].data.num_comments;					
 
-				var dogStory = $("<h1></h1>").html("<a href='" + imgUrl + "'>" + title + "</a>");
-				$('body').append(dogStory);
+				var dogStory = $("<h2></h2>").html("<a href='" + imgUrl + "'>" + title + "</a>");
+				// $("body").append(dogStory);
+
+				var commentSection = $("<p></p>").html(numComments + " " + "<a href='http://www.reddit.com/" + comments + "'>comments</a>");
+
+				var authorName = $("<h3></h3>").html(author);
 
 				var url = somethingElse.data.children[i].data.thumbnail;
 				var dogImage = $("<img />").attr("src" , url);
 
-				$('body').append([dogStory, dogImage]);
+				var divsection = $('<div class="divclass"></div>');
 
+				divsection.append([dogStory, authorName, dogImage, commentSection]);
+
+				$("body").append(divsection);
 			}
 				// if(i === 11) break;
 		}
