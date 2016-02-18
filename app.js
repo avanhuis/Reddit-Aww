039ebbd1978e32dec042d1be3e529462e5224ad1
Amanda Milke <amandalmilke@gmail.com>
2016-02-18 14:55:56 -0500

    made the headings links

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ $(function(){
 			for(i = 0; i < 10; i++) {
 					// $(".x1").append(x3);
 				var title = somethingElse.data.children[i].data.title;
+				var imgUrl = somethingElse.data.children[i].data.url;
 
-				var dogStory = $("<h1></h1>").text(title);
+				var dogStory = $("<h1></h1>").html("<a href='" + imgUrl + "'>" + title + "</a>");
 				$('body').append(dogStory);
 
 				var url = somethingElse.data.children[i].data.thumbnail;
